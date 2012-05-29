6359a4ce568f4142e21674998e76326276ae60c4
Ricky Chilcott <ricky@rbsavvy.com>
2012-05-29 00:49:21 -0400

    Initial markets controller work

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,5 +1,12 @@
 class MarketsController < ApplicationController
   def index
+    if params[:lat] && params[:long]
+      @markets = Market.geo_near(:lat => params[:lat], :lng => params[:lng])
+    end
+    
+    @markets ||= Market.all
+    
+    @markets = @markets.limit(10)
   end
 
   def show
--- app/views/markets/show.json.rabl
+++ app/views/markets/show.json.rabl
@@ -1,2 +1,5 @@
 object @market
-attributes :id, :name
+attributes :id, :name, :street, :city, :county, :state, :zip, :description, :credit_card, :wic, :wic_cash, :sfmnp, :snap
+
+node(:lat) { |market| market.location[:lat] }
+node(:long) { |market| market.location[:lng] }
